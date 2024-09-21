73f43b5efd3011f8f0a221c61ec7c15350f12bfd
ElGhostWolf <Hmjuan2108@gmail.com>
2024-09-21 11:42:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const botonMostrar = document.getElementById('mostrar-imagenes');
 const imageContainer = document.querySelector('.image-container');
 const descripcion = document.getElementById('descripcion');
 
-// Mostrar la imagen y la descripción al hacer clic en "Presiona aquí"
 botonMostrar.addEventListener('click', () => {
     mensajeInicial.style.display = 'none';
     botonMostrar.style.display = 'none';
